65a478ba5e211d1348699635d41b42091a7f9909
s1mplycompl3x@outlook.com <imi@Owner.local>
2017-05-30 17:39:42 -0400

    Initial Commit

---
+++ easymp3.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+Add read from text file ability 
+Add argument for showing longer results list  ✅
+Add safeguards for the input ✅
+Add ability to pick output file name
+Add download Audio or Video option
+Add Automode (download without asking which one)✅
+"""
+import sys, os, webbrowser, requests, youtube_dl, argparse
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+#CONDITIONS
+parser = argparse.ArgumentParser(description='Downloading and converting videos2mp3 made SIMPLE!')
+parser.add_argument("Name", help="Put the name of the song and/or artist here")
+parser.add_argument("-s", "--size", type=int, help="How many options to show for download, (Default = 10)")
+parser.add_argument("-a", "--auto", action="store_true", help="Run the script Automatic without asking for an option")
+args = parser.parse_args()
+if args.size:
+	list_size = args.size
+elif args.size is None:
+	list_size = 10
+if args.Name:
+	get_input = args.Name
+if args.auto:
+	run_auto = 1
+
+#-----------------------------------------------------------------
+query = quote(get_input)
+res = requests.get('https://youtube.com/results?search_query=' + query)
+soup = BeautifulSoup(res.text, "html.parser")
+url = ''
+list = []
+print('\n\n')
+for index, vid in enumerate(soup.findAll(attrs={'class' : 'yt-uix-tile-link'})[:list_size], 1):
+	if (run_auto is not 1):
+		print(''+str(index)+'. ' + vid['title'])
+	temp_url = 'https://youtube.com' + vid['href']
+	list.insert((index), temp_url)
+if run_auto is not 1:
+	valid = 0
+	while (valid == 0):
+		choice = int(input('\nEnter your choice: '))
+		if choice > 0 and choice < 11:
+			valid = 1
+		else:
+			print('invalid choice, try again!')
+	choice -=1
+	url = list[choice]
+elif run_auto == 1:
+	url = list[1]
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+	ydl.download([url])
+
